bd4a33828a573cf4d3dc7e2bc6fa5d80d3cef457
Jack White <jfw793@gmail.com>
2024-02-25 21:51:50 +1100

    add daily api extraction dag

---
+++ airflow/dags/ExtractApiData.py
@@ -0,0 +1,29 @@
+from airflow.decorators import dag, task
+from pendulum import datetime, duration
+from scripts.extract_data import extract_data
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": duration(minutes=1),
+}
+
+
+@dag(
+    start_date=datetime(2024, 2, 24),
+    schedule="@daily",
+    default_args=default_args,
+)
+def extract_api_data():
+    @task(task_id="extract_data")
+    def extract_data_task():
+        extract_data()
+        return "Extracted data successfully"
+
+    extract_data_task()
+
+
+extract_api_data()
