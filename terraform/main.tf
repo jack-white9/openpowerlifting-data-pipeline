07265191585496b27e8a345ffd678b2f64df6d02
Jack White <jfw793@gmail.com>
2024-02-28 13:14:32 +1100

    add expiration action to s3 objects

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,20 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_s3_bucket" "bronze" {
-  bucket = "openpowerlifting-data-bronze"
+resource "aws_s3_bucket" "raw" {
+  bucket = "openpowerlifting-data-raw"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
+  bucket = aws_s3_bucket.raw.id
+
+  rule {
+    id = "expire"
+
+    expiration {
+      days = 10
+    }
+
+    status = "Enabled"
+  }
 }
